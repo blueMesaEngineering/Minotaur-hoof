72ffebc199c3afef864af53df22bc9cd84b7fb7e
Death <mathrox431@yahoo.com>
2019-03-07 20:23:48 -0500

    Style and page updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'static_pages#home'
+  get  '/about',   		     	to: 'static_pages#about'
+  get  '/contact', 		     	to: 'static_pages#contact'
+  get  '/new_model',			to: 'url_data_models#new'
 
   resources :url_data_models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
